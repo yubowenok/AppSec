7fc176e7f87f8cdf6f33208850266c1ea023ed22
Bowen Yu <yubowenok@gmail.com>
2013-02-12 15:14:03 -0500

    Makefile

--- SecSandbox/SecSandbox/abort.cpp
+++ SecSandbox/SecSandbox/abort.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+
 #include "sandbox.h"
 
 // abort with error
--- SecSandbox/SecSandbox/main.cpp
+++ SecSandbox/SecSandbox/main.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <cstdlib>
 
 #include "sandbox.h"
 
@@ -15,8 +16,8 @@ int main(int argc, char *argv[]){
 	// check the arguments
 	if(argc!=2 && argc!=3){
 		cout << "Insufficient args. Usage: ./sandbox argprog argdata" << endl;
-		exit(0);
-	}else if(argc==2){
+	    exit(0);
+    }else if(argc==2){
 		cout << "Running program \"" << argv[1] << "\"..." << endl;
 	}else if(argc==3){
 		cout << "Running program \"" << argv[1] << "\" with data \"" << argv[2] << "\"..." << endl;
@@ -26,4 +27,6 @@ int main(int argc, char *argv[]){
 
 	// execute the sandbox
 	box.exec();
+
+    return 0;
 }
